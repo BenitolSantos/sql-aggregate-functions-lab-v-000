ce25f0c1bbf413c87771be85000917a496b855fe
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-03 15:41:09 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -20,7 +20,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  ""
+  "SELECT TOTAL(tardies) AS total_tardies_for_all_students FROM students;"
 end
 
 def average_gpa_for_9th_grade
